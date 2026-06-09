f9733f279cd0e1a2a1506f3306b88afdf4e2898a
ChargePulse System <chargepulse.system@gmail.com>
2026-06-09 19:23:15 +0200

    style: add responsive padding to main dashboard content

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -45,8 +45,9 @@ export default function AdminLayout({ children, user, branches, activeBranchId }
           onOpenMobile={() => setIsMobileMenuOpen(true)}
           userInitials={user.initials}
         />
-        
-        {children}
+        <main className="flex-1 px-4 sm:px-6 lg:px-8 py-6">
+          {children}
+        </main>
       </div>
     </div>
   )
